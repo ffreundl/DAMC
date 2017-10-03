c8df4568d4022c7649e4bc357395c766cf4a1aed
Théo Imler <theo.imler@epfl.ch>
2017-10-03 14:38:40 +0200

    new plot

--- Miniproject_1/WORKSPACE/Miniproject1.m
+++ Miniproject_1/WORKSPACE/Miniproject1.m
@@ -14,3 +14,33 @@ a1 = plot(mean1); M1 = 'Cluster 1';
 a2 = plot(mean2); M2 = 'Cluster 2';
 a3 = plot(mean3); M3 = 'Cluster 3';
 legend([a1,a2,a3], M1, M2, M3)
+
+figure;
+subplot(2,3,1);
+for i = cluster1(1):cluster1(size(cluster1)) 
+    plot(spikes(i,:))
+    hold on
+end
+subplot(2,3,2);
+for i = cluster2(1):cluster2(size(cluster2)) 
+    plot(spikes(i,:))
+    hold on
+end
+i
+subplot(2,3,3);
+for i = cluster3(1):cluster3(size(cluster3)) 
+    plot(spikes(i,:))
+    hold on
+end
+i
+subplot(2,3,4);
+a1 = plot(mean1); M1 = 'Cluster 1';
+hold on;
+
+subplot(2,3,5);
+a2 = plot(mean2); M2 = 'Cluster 2';
+hold on;
+
+subplot(2,3,6);
+a3 = plot(mean3); M3 = 'Cluster 3';
+hold on;
