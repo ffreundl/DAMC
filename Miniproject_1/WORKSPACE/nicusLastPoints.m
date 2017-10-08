45887f2c4abed499d9d11c375bbe1d6fd228fd3a
nfreundl <nicolas.freundler@epfl.ch>
2017-10-08 18:17:21 +0200

    dernier point, reste à discuter lequel est le meilleur

--- Miniproject_1/WORKSPACE/nicusLastPoints.m
+++ Miniproject_1/WORKSPACE/nicusLastPoints.m
@@ -33,7 +33,7 @@ disp(D);
 %There is two things we want to minimize : the sum of the point-to-centroid
 %distance sums (considered as an error measurement) and the number of cluster
 % (a big number gives a big complexity, until the preposterous case of k=6000)
-%%
+%%Last point
 %last part, finding the best number of groups regarding different criteria
 %MATLAB documentation give four criteria
 %CalinskiHarabasz
@@ -48,6 +48,13 @@ DavBou=evalclusters(spikesPCA,IDX,'DaviesBouldin');
 
 optimalValues=[CalHar.OptimalK, Sil.OptimalK, gap.OptimalK, DavBou.OptimalK];
 
-for indx=1:4
-	fprintf('for %s criterion the optimal value is %d\n\n',names(indx),optimalValues(indx));
-end
+%The following definitions come from the MATLAB documentation
+%CalinskiHarabasz is a criterion based on variances. The value to minimize is the ratio of overall 
+%between-cluster variance and the overall within-cluster varance, times (N-k)/(k-1).
+%With the silhouette criterion, one minimizes the value of the same name : S = (b-a)/max(a,b)
+%For a point a is the average distance to the other points of the same cluster and b the minimum
+%distance to another point from a different cluster (max(a,b) seems to be here to normalize the
+%range from -1 to 1). For a high value, points are near each other within a clusters, but far away
+%from other clusters, then the clustering is consistent. Else, a negative value will show inconsistency.
+%(Does matlab compute the silhouette for every points ?!? that would explain chronophagy)
+%Gap criterion maximizes the value Gap=Expatation(log(Wk))-log(Wk) where Wk is pooled within-cluster measurment.
